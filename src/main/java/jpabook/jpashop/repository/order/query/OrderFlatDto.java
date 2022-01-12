b56d2717df469861655c773621d26c7c1e6d9fd6
Leeky0615 <rbdus7174@naver.com>
2022-01-12 16:45:23 +0900

    feat : learn JPA direct dto inquiry (V6: flat data optimization)
    장점은 쿼리 1번 / 단점은 중복데이터가 추가됨, 애플리케이션 추가작업이 크다, 페이징 불가능

--- src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@EqualsAndHashCode(of = "orderId") //orderId를 기준으로 Collector에서 groupby를 할때 묶어준다.
-public class OrderQueryDto {
+public class OrderFlatDto {
 
     private Long orderId;
     private String name;
@@ -19,22 +17,19 @@ public class OrderQueryDto {
     private LocalDateTime orderDate;
     private OrderStatus orderStatus;
     private Address address;
-    private List<OrderItemQueryDto> orderItems;
 
-    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+    private String itemName;
+    private int orderPrice;
+    private int count;
+
+    public OrderFlatDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, String itemName, int orderPrice, int count) {
         this.orderId = orderId;
         this.name = name;
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
-    }
-
-    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
-        this.orderId = orderId;
-        this.name = name;
-        this.orderDate = orderDate;
-        this.orderStatus = orderStatus;
-        this.address = address;
-        this.orderItems = orderItems;
+        this.itemName = itemName;
+        this.orderPrice = orderPrice;
+        this.count = count;
     }
 }
